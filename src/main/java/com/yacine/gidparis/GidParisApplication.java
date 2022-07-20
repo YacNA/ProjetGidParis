c9901688c7a5bf85fec4fefe669d02f30e6350d5
yacine <yacinenaitamer@yahoo.fr>
2022-07-20 08:45:11 +0200

    modif un site

--- src/main/java/com/yacine/gidparis/GidParisApplication.java
+++ src/main/java/com/yacine/gidparis/GidParisApplication.java
@@ -2,12 +2,14 @@ package com.yacine.gidparis;
 
 import com.yacine.gidparis.dao.SitesRepository;
 import com.yacine.gidparis.entities.Sites;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
 public class GidParisApplication {
@@ -23,10 +25,11 @@ public class GidParisApplication {
 			/*Sites sites = new Sites();
 			sites.setId(12l);
 			Sites s1= sitesRepository.save(sites);*/
-			Sites s1 = sitesRepository.save(new Sites(1L,"Tour Eiffel",4,4,4,"Champ de Mars, 5av.Anatole France, 75007 Paris",930,2230));
-			Sites s2 = sitesRepository.save(new Sites(2L,"Cathédrale Notre-Dame de Paris",4,4,4,"6 Parvis Notre-Dame-Pl.Jean-Paul ||, 75004 Paris",0,0));
-			Sites s3 = sitesRepository.save(new Sites(3L,"Sacré-Coeur",4,4,4,"35 Rue du Chevalier de la Barre, 75018 Paris",6,22));
-			Sites s4 = sitesRepository.save(new Sites(4L,"Musée du louvre",4,4,4,"Rue de Rivoli, 75001 Paris",9,18));
+			DateFormat Tf=new SimpleDateFormat("HH:mm");
+			Sites s1 = sitesRepository.save(new Sites(1L,"Tour Eiffel",4,4,4,"Champ de Mars, 5av.Anatole France, 75007 Paris","9:30","22:30"));
+			Sites s2 = sitesRepository.save(new Sites(2L,"Cathédrale Notre-Dame de Paris",4,4,4,"6 Parvis Notre-Dame-Pl.Jean-Paul ||, 75004 Paris","0","0"));
+			Sites s3 = sitesRepository.save(new Sites(3L,"Sacré-Coeur",4,4,4,"35 Rue du Chevalier de la Barre, 75018 Paris","6:00","22:00"));
+			Sites s4 = sitesRepository.save(new Sites(4L,"Saint-michel",4,4,4,"Rue de Rivoli, 75001 Paris","9:00","18:00"));
 
 		};
 	}
--- src/main/java/com/yacine/gidparis/entities/Sites.java
+++ src/main/java/com/yacine/gidparis/entities/Sites.java
@@ -1,27 +1,24 @@
 package com.yacine.gidparis.entities;
 
-import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@AllArgsConstructor @NoArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Sites implements Serializable {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String designation;
     private double longitude,latitude,altitude;
     private String adresse;
-    @Timestamp
-    private int heureOuverture;
-    private int heureFeremeture;
+    @DateTimeFormat
+    private String heureOuverture;
+    private String heureFeremeture;
 
 
 }
